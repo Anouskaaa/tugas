0db15b5eace7ecae365eeed0796793dff6adc2a8
Anouskaaa <88115874+Anouskaaa@users.noreply.github.com>
2023-03-30 18:05:51 +0700

    Update webscrapper.py

--- webscrapper.py
+++ webscrapper.py
@@ -1,7 +1,5 @@
-#import library
 from bs4 import BeautifulSoup
 import requests
-#request to website and download HTML contents
 url='https://id.jobsdb.com/id'
 req=requests.get(url)
 content=req.text
